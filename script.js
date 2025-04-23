d988a6e46e16cb29fb23a103b430d1e953d6be9b
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-23 12:42:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here!
+const list=document.getElementById("infi-list");
 
+function createListitem(index){
+	const li=document.createElement('li');
+	li.textContent=`Item ${index}`;
+	return li;
+}
+
+let itemcount=10;
+for(let i=1; i<itemcount; i++){
+	list.appendChild(createListitem(i));
+}
+
+function addmoreItem(){
+	for(let i=1; i<=2; i++){
+		itemcount++;
+		list.appendChild(createListitem(itemcount));
+	}
+}
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    addmoreItem(); // user reached bottom, so add more items
+  }
+});
